e3e34dcf93137fa59248ed2b38484ff94cec8874
vikas <vikaskonda.vicky@gmail.com>
2025-07-21 20:17:58 +0530

    creating catalouge file using shell script

--- catlouge.sh
+++ catlouge.sh
@@ -80,7 +80,7 @@ VALIDATE $? "Installing MongoDB Client"
 STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
 then
-    mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
+    mongosh --host mongodb.vikaskonda.in </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
 else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
