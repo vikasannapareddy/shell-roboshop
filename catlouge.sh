ef7681a3e5a3000d02429b20262a1260ec752862
vikas <vikaskonda.vicky@gmail.com>
2025-07-21 20:08:47 +0530

    dding catlouge file

--- catlouge.sh
+++ catlouge.sh
@@ -1,58 +1,87 @@
-#!bin/bash
+#!/bin/bash
+
 USERID=$(id -u)
-# Check if the script is run as root
-
 R="\e[31m"
-# red color
 G="\e[32m"
-# green color
 Y="\e[33m"
-# yellow color
-B="\e[34m"
-# blue color
-M="\e[35m"
-# magenta color
-C="\e[36m"
-# cyan color
-W="\e[37m"
-# white color
 N="\e[0m"
-# no color
-
 LOGS_FOLDER="/var/log/roboshop-logs"
-# Check if the logs folder exists
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-# Get the script name without extension
-LOG_FILE="${LOGS_FOLDER}/${SCRIPT_NAME}.log"
-# Log file path
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
-# Current working directory
 
 mkdir -p $LOGS_FOLDER
-echo "script started executing at: $(date)" | tee -a $LOG_FILE
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-if [ $USERID -ne 0 ]; then
-  echo -e "$R ERROR:: Please run this script with root accesss $N" | tee -a $LOG_FILE
-  exit 1
+# check the user has root priveleges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
 else
-    echo "you are running with root access" | tee -a $LOG_FILE
-
+    echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
+# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
         echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-        #
     else
         echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
     fi
 }
+
 dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "Disabling NodeJS module"
+VALIDATE $? "Disabling default nodejs"
 
 dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? "Enabling NodeJS:20 module"
+VALIDATE $? "Enabling nodejs:20"
 
 dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "Installing NodeJS:20"
+VALIDATE $? "Installing nodejs:20"
+
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating roboshop system user"
+else
+    echo -e "System user roboshop already created ... $Y SKIPPING $N"
+fi
+
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading Catalogue"
+
+rm -rf /app/*
+cd /app 
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "unzipping catalogue"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "Installing Dependencies"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Copying catalogue service"
+
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable catalogue  &>>$LOG_FILE
+systemctl start catalogue
+VALIDATE $? "Starting Catalogue"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB Client"
+
+STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ $STATUS -lt 0 ]
+then
+    mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Loading data into MongoDB"
+else
+    echo -e "Data is already loaded ... $Y SKIPPING $N"
+fi
